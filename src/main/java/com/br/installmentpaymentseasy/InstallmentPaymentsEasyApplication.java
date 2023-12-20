fc638fd4b1fc164a6efcac0be835c4b979cf6b3d
Christian Catrofe <christianlopes100@outlook.com>
2023-12-20 14:00:29 -0300

    First Commit

---
+++ src/main/java/com/br/installmentpaymentseasy/InstallmentPaymentsEasyApplication.java
@@ -0,0 +1,13 @@
+package com.br.installmentpaymentseasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallmentPaymentsEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstallmentPaymentsEasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/installmentpaymentseasy/InstallmentPaymentsEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.installmentpaymentseasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstallmentPaymentsEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
